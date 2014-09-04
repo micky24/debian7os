de3572366459d06c636814bc3195cc42120f007a
micky24 <micky.teguh@gmail.com>
2014-09-04 16:28:17 +0700

    Update autokill.sh

--- autokill.sh
+++ autokill.sh
@@ -1,13 +1,8 @@
-#while :
- # do 
-  #./userlimit.sh 1 
-  #sleep 5 
-  #done
-#while :
-  #do
-  #./userlimit.sh 2
-  #sleep 55 
-  #done
+while :
+  do
+  ./userlimit.sh 2
+  sleep 55 
+  done
 while :
   do
   ./userexpired.sh
