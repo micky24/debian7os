ad52ee7543b362d4bbc198ef5b10299d4c7d4d20
micky24 <micky.teguh@gmail.com>
2014-12-27 06:07:20 +0700

    Update uptime.php

--- uptime.php
+++ uptime.php
@@ -1,4 +1,51 @@
 <?php
+$dl = 90;
+$wl = 75;
+$memloc = '/proc/meminfo'; 
+$fileloc = '/'; 
+$uptimeloc = '/proc/uptime'; 
+$loadtime = 0;
+$post = array(); 
+$internal = array();
+$internal['uptime'] = explode('.', file_get_contents($uptimeloc), 2);
+$internal['memraw'] = file_get_contents($memloc);
+$internal['hddtotal'] = disk_total_space($fileloc);
+$internal['hddfree'] = disk_free_space($fileloc);
+$internal['load'] = sys_getloadavg();
+$post['uptime'] = sec2human($internal['uptime'][0]); // Processing uptime and putting in post field!
+preg_match_all('/MemTotal:(.*)kB/', $internal['memraw'], $internal['memtotal']); // Get Total Memory!
+$internal['memtotal'] = trim($internal['memtotal'][1][0], " ");  // Make nice.
+preg_match_all('/MemFree:(.*)kB/', $internal['memraw'], $internal['memfree']); // Get Free Memory!
+$internal['memfree'] = trim($internal['memfree'][1][0], " "); // Make nice.
+preg_match_all('/Cached:(.*)kB/', $internal['memraw'], $internal['memcache']); // Get Cached Memory!
+$internal['memfree'] = trim($internal['memcache'][1][0], " ") + $internal['memfree']; // Making cache seen as Free Memory!
+$internal['memperc'] = round((($internal['memtotal'] - $internal['memfree']) / $internal['memtotal']) * 100); // calculations
+$post['memory'] = levels($internal['memperc'], $dl, $wl);  // adding to the post field!
+$internal['hddperc'] = round((($internal['hddtotal'] - $internal['hddfree']) / $internal['hddtotal']) * 100); // calculations!
+$post['hdd'] = levels($internal['hddperc'], $dl, $wl); // adding hdd to the post field!
+$post['load'] = $internal['load'][$loadtime]; // posting load avg.
+$post['online'] = '<div class="progress"><div class="bar bar-success" style="width: 100%;"><small>Up</small></div></div>';
+echo json_encode($post); // Time to show the world what we are made of!
+function levels($perc, $dl, $wl){
+    // make nice green bars
+    if($perc < 30) {
+        $width = 30;
+    } else {
+        $width = $perc;
+    }
+    if($perc < $wl) { 
+        $return = '<div class="progress progress-striped active"><div class="bar bar-success" style="width: ' . $width . '%;">' . $perc . '%</div</div>';
+    }
+    elseif($perc > $wl) {
+        $return = '<div class="progress progress-striped active"><div class="bar bar-warning" style="width: ' . $width . '%;">' . $perc . '%</div></div>';
+    }
+    elseif($perc > $dl) {
+        $return = '<div class="progress progress-striped active"><div class="bar bar-danger" style="width: ' . $width . '%;">' . $perc . '%</div></div>';
+    }
+    return $return;
+    
+}
+
 function sec2human($time) {
   $seconds = $time%60;
 	$mins = floor($time/60)%60;
@@ -6,51 +53,5 @@ function sec2human($time) {
 	$days = floor($time/60/60/24);
 	return $days > 0 ? $days . ' day'.($days > 1 ? 's' : '') : $hours.':'.$mins.':'.$seconds;
 }
-$array = array();
-$fh = fopen('/proc/uptime', 'r');
-$uptime = fgets($fh);
-fclose($fh);
-$uptime = explode('.', $uptime, 2);
-$array['uptime'] = sec2human($uptime[0]);
-$fh = fopen('/proc/meminfo', 'r');
-  $mem = 0;
-  while ($line = fgets($fh)) {
-    $pieces = array();
-    if (preg_match('/^MemTotal:\s+(\d+)\skB$/', $line, $pieces)) {
-      $memtotal = $pieces[1];
-    }
-    if (preg_match('/^MemFree:\s+(\d+)\skB$/', $line, $pieces)) {
-      $memfree = $pieces[1];
-    }
-    if (preg_match('/^Cached:\s+(\d+)\skB$/', $line, $pieces)) {
-      $memcache = $pieces[1];
-      break;
-    }
-  }
-fclose($fh);
-$memmath = $memcache + $memfree;
-$memmath2 = $memmath / $memtotal * 100;
-$memory = round($memmath2) . '%';
-if ($memory >= "51%") { $memlevel = "success"; }
-elseif ($memory <= "50%") { $memlevel = "warning"; }
-elseif ($memory <= "35%") { $memlevel = "danger"; }
-$array['memory'] = '<div class="progress progress-striped active">
-<div class="bar bar-'.$memlevel.'" style="width: '.$memory.';">'.$memory.'</div>
-</div>';
-$hddtotal = disk_total_space("/");
-$hddfree = disk_free_space("/");
-$hddmath = $hddfree / $hddtotal * 100;
-$hdd = round($hddmath) . '%';
-if ($hdd >= "51%") { $hddlevel = "success"; }
-elseif ($hdd <= "50%") { $hddlevel = "warning"; }
-elseif ($hdd <= "35%") { $hddlevel = "danger"; }
-$array['hdd'] = '<div class="progress progress-striped active">
-<div class="bar bar-'.$hddlevel.'" style="width: '.$hdd.';">'.$hdd.'</div>
-</div>';
-$load = sys_getloadavg();
-$array['load'] = $load[0];
-$array['online'] = '<div class="progress">
-<div class="bar bar-success" style="width: 100%;"><small>Up</small></div>
-</div>';
 
-echo json_encode($array);
+?>
